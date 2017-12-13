ab6244c2a53ba457bf8ab6a8d94eaf871c106c96
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-13 17:57:46 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,7 +33,7 @@ def hit?(card_total)
   prompt_user
   input=get_user_input
   if input=="h"
-    card_total+=deal_card
+    return card_total+=deal_card
   elsif input=="s"
     return card_total
   else
